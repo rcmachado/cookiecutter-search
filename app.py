79848cb5f81dace58dfbabaf55a509568f11d8df
Rodrigo Machado <rcmachado@gmail.com>
2014-11-07 17:21:40 -0300

    Add a very simple cache logic

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pylibmc
 import requests
 from requests.auth import HTTPBasicAuth
 from flask import Flask, request, render_template, jsonify
@@ -5,7 +6,8 @@ from flask import Flask, request, render_template, jsonify
 import config
 
 app = Flask(__name__)
-
+cache = pylibmc.Client(config.MEMCACHE['SERVERS'], binary=True,
+                       behaviors=config.MEMCACHE['OPTIONS'])
 
 @app.route("/")
 def index():
@@ -15,7 +17,20 @@ def index():
 @app.route("/search")
 def search():
     search_term = request.args.get('q')
+    response = _search_for_term(search_term)
+    return jsonify(response)
 
+
+def _search_for_term(search_term):
+    cache_key = 's-{}'.format(search_term)
+    response = cache.get(cache_key)
+    if not response:
+        response = _make_request(search_term)
+        cache.set(cache_key, response, 3600)
+    return response
+
+
+def _make_request(search_term):
     basic_auth = HTTPBasicAuth(config.GITHUB_TOKEN, 'x-oauth-basic')
     url = ('https://api.github.com/search/repositories?q="cookiecutter%20'
            'template"+{term}+in:description&sort=stars&order=desc')
@@ -23,6 +38,21 @@ def search():
     github_response = requests.get(url.format(term=search_term),
                                    auth=basic_auth)
 
+    if github_response.status_code == 401:
+        response = {
+            'error': "We're over quota. Please come back after a few minutes :)"
+        }
+    elif github_response.status_code == 200:
+        response = _transform_response(github_response)
+    else:
+        response = {
+            'error': 'An unknown error ocurred: {}'.format(github_response.status_code)
+        }
+
+    return response
+
+
+def _transform_response(github_response):
     response = []
     items = github_response.json().get('items', [])
     for item in items:
@@ -31,9 +61,9 @@ def search():
             'description': item['description'],
             'url': item['html_url'],
         })
-    return jsonify({'results': response})
+    return {'results': response}
 
 
 if __name__ == "__main__":
-    app.debug = config.DEBUG
+    app.debug = True #config.DEBUG
     app.run()
--- config.py
+++ config.py
@@ -3,3 +3,19 @@ import os
 
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 DEBUG = os.environ.get('DEBUG', False)
+
+MEMCACHE = {
+    'SERVERS': os.environ.get('MEMCACHIER_SERVERS', '').split(','),
+    'USERNAME': os.environ.get('MEMCACHIER_USERNAME', ''),
+    'PASSWORD': os.environ.get('MEMCACHIER_PASSWORD', ''),
+    # These came from https://devcenter.heroku.com/articles/memcachier#django
+    'OPTIONS': {
+        'no_block': True,
+        'tcp_nodelay': True,
+        'tcp_keepalive': True,
+        'remove_failed': 4,
+        'retry_timeout': 2,
+        'dead_timeout': 10,
+        '_poll_timeout': 2000,
+    }
+}
