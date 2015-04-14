dd6e9055f216df6c943dbb563c76afa5d9572717
Rodrigo Machado <rcmachado@gmail.com>
2015-04-13 22:56:04 -0300

    Respect DEBUG and PORT config directives

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 # coding: utf-8
 from tornado.ioloop import IOLoop
 
+from cookiecutter_search import config
 from cookiecutter_search.application import MainApplication
 from cookiecutter_search.urls import urlpatterns
 
 
 if __name__ == '__main__':
-    config = {
+    app_config = {
+        'debug': config.DEBUG,
         'static_path': 'cookiecutter_search/static/',
         'template_path': 'cookiecutter_search/templates/'
     }
-    app = MainApplication(urlpatterns, **config)
-    app.listen(8888)
+    app = MainApplication(urlpatterns, **app_config)
+    app.listen(config.PORT)
     IOLoop.instance().start()
